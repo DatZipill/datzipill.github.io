e000e32c8eec327ec375d1d613ec5d3cb0d607de
DatZipill <101321349+DatZipill@users.noreply.github.com>
2022-12-02 00:02:40 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const html_btn_no = document.getElementById("btn-no");
+html_btn_no.addEventListener("mouseover", moveHover);
+const html_btn_yes = document.getElementById("btn-yes");
+html_btn_yes.addEventListener("click", btn_yes_clicked);
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function moveHover() {
+  const i = Math.floor(getRndInteger(html_btn_no.offsetWidth ,w)-html_btn_no.offsetWidth);
+  const j = Math.floor(getRndInteger(html_btn_no.offsetHeight ,h)-html_btn_no.offsetHeight);
+
+  html_btn_no.style.left = i+"px"
+  html_btn_no.style.top = j+"px"
+  html_btn_no.style.position = "absolute"
+
+  html_btn_yes.textContent = "Thôi ấn nút này đi nè"
+} 
+
+function btn_yes_clicked() {
+  const open_web_timeout = setTimeout(open_web, 2000);
+  html_btn_yes.textContent = "Ib ngay còn gì nữa"
+}
+
+function open_web() {
+  window.open("https://www.facebook.com/profile.php?id=100030532059237")  
+}
